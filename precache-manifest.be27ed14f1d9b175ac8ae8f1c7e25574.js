9b46532631ef0c61c5127252b12edc7d96287e25
desmond93 <bondarenkomaxim93@gmail.com>
2020-10-29 13:30:34 +0200

    Updates

---
+++ precache-manifest.be27ed14f1d9b175ac8ae8f1c7e25574.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09b2eeb150e0e59ece674836f46ff772",
+    "url": "/crackerApp_ReactNew/index.html"
+  },
+  {
+    "revision": "de64806ceda37bfbf660",
+    "url": "/crackerApp_ReactNew/static/css/main.93c197bb.chunk.css"
+  },
+  {
+    "revision": "d5a7167e1171e68336b1",
+    "url": "/crackerApp_ReactNew/static/js/2.f36f7cef.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/crackerApp_ReactNew/static/js/2.f36f7cef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de64806ceda37bfbf660",
+    "url": "/crackerApp_ReactNew/static/js/main.20022526.chunk.js"
+  },
+  {
+    "revision": "d77b85f05c5f36452241",
+    "url": "/crackerApp_ReactNew/static/js/runtime-main.9ebe6302.js"
+  },
+  {
+    "revision": "5894b9ba5177a3f47aa9c86306abc729",
+    "url": "/crackerApp_ReactNew/static/media/FirstSection_background.5894b9ba.png"
+  },
+  {
+    "revision": "e768202b05697f8f0dff6ddf93deadf6",
+    "url": "/crackerApp_ReactNew/static/media/Footer_background.e768202b.png"
+  },
+  {
+    "revision": "0b861ba80ccee6ae4385136bc6fdebcb",
+    "url": "/crackerApp_ReactNew/static/media/SecondSection_background.0b861ba8.png"
+  },
+  {
+    "revision": "46332b7f812f29c86f44fd9043c56d0b",
+    "url": "/crackerApp_ReactNew/static/media/SecondSection_circleCracker.46332b7f.png"
+  }
+]);
